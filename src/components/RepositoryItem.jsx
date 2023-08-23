c877714a53bbdf7cc373d82fb9ec05d752d56297
Tommy Nguyen <tommynguyen.164@gmail.com>
2023-08-23 15:43:31 -0700

    Add initial repositorylist

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native';
+
+const RepositoryItem = ({ 
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazersCount,
+  ratingAverage,
+  reviewCount,
+}) => {
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating Average: {ratingAverage}</Text>
+    </View>
+  )
+}
+
+export default RepositoryItem
